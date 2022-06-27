8ea7406c499dc370a729f11c6a56079eb0bd7575
donno2048 <just4now666666@gmail.com>
2022-06-27 12:49:12 +0300

    don't use audio as video

--- link2mp4/reddit.py
+++ link2mp4/reddit.py
@@ -2,7 +2,7 @@ from .capture import requests, video
 def download():
     id = input('https://www.reddit.com/r/')
     req = requests('https://www.reddit.com/r/%s' % id)
-    vid = max(filter(lambda req: '.mp4' in req, req))
+    vid = max(filter(lambda req: '.mp4' in req and '_audio.mp4' not in req, req))
     try:
         aud = list(filter(lambda req: '_audio.mp4' in req, req))[0]
     except IndexError:
