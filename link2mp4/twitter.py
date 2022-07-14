590b71d1a155d2f06e25cf7c049f4c0349766ffb
donno2048 <just4now666666@gmail.com>
2022-07-14 21:54:18 +0300

    remove `requests`

--- link2mp4/twitter.py
+++ link2mp4/twitter.py
@@ -1,5 +1,5 @@
 from urllib.parse import urlparse
-from requests import get
+from urllib3 import PoolManager
 from m3u8 import loads
 try:
     from .capture import requests
@@ -8,12 +8,13 @@ except ImportError:
 def download():
     id = input('https://twitter.com/')
     req = requests('https://twitter.com/%s' % id)
+    pool_manager = PoolManager()
     playlist_source = list(filter(lambda req: '.m3u8' in req, req.copy()))[0]
     host = urlparse(playlist_source)
     host_url = '%s://%s' % (host.scheme, host.hostname)
-    playlists_text = get(playlist_source).text
+    playlists_text = pool_manager.request('GET', playlist_source).data.decode()
     playlists = loads(playlists_text)
     if playlists.playlists:
-        playlists_text = get(host_url + max(playlists.playlists, key=lambda x: x.stream_info.resolution).uri).text
+        playlists_text = pool_manager.request('GET', host_url + max(playlists.playlists, key=lambda x: x.stream_info.resolution).uri).data.decode()
         playlists = loads(playlists_text)
-    return get(host_url + playlists_text[playlists_text.find('"') + 1: playlists_text.rfind('"')]).content + b''.join(map(lambda x: get(host_url + x).content, playlists.segments.uri))
+    return pool_manager.request('GET', host_url + playlists_text[playlists_text.find('"') + 1: playlists_text.rfind('"')]).data + b''.join(map(lambda x: pool_manager.request('GET', host_url + x).data, playlists.segments.uri))
