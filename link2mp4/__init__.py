63969608d63325f2ae1671958fe5d4e773a00e6a
donno2048 <just4now666666@gmail.com>
2022-07-24 15:04:50 +0300

    add vimeo

--- link2mp4/__init__.py
+++ link2mp4/__init__.py
@@ -1,11 +1,12 @@
 try:
-    from . import youtube, reddit, twitter, pinterest, tiktok
+    from . import youtube, reddit, twitter, pinterest, tiktok, vimeo
 except ImportError:
-    import youtube, reddit, twitter, pinterest, tiktok
+    import youtube, reddit, twitter, pinterest, tiktok, vimeo
 download_functions = {
     'YouTube': youtube.download,
     'Reddit': reddit.download,
     'Twitter': twitter.download,
     'Pinterest': pinterest.download,
-    'TikTok': tiktok.download
+    'TikTok': tiktok.download,
+    'Vimeo': vimeo.download
 }
